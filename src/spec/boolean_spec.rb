b7d610d019d907073841f04ab59eb87262b5bf90
Alejandro <aleperaltabazas@hotmail.com>
2019-01-01 02:38:21 -0300

    adding some more tests

--- src/spec/boolean_spec.rb
+++ src/spec/boolean_spec.rb
@@ -3,30 +3,62 @@ require_relative '../lib/boolean'
 
 describe 'Boolean spec' do
   it 'true and false should be false' do
-    p = true
-    q = false
-
-    expect(p.and(q)).to be_falsey
+    expect(true.and(false)).to be_falsey
   end
 
   it 'true or false should be true' do
-    p = true
-    q = false
+    expect(true.or(false)).to be_truthy
+  end
 
-    expect(p.or(q)).to be_truthy
+  it 'true or true should be true' do
+    expect(true.or(true)).to be_truthy
+  end
+
+  it 'false or false should be false' do
+    expect(false.or(false)).to be_falsey
   end
 
   it 'true xor false should be true' do
-    p = true
-    q = false
-
-    expect(p.xor(q)).to be_truthy
+    expect(true.xor(false)).to be_truthy
   end
 
   it 'true xor true should be false' do
-    p = true
-    q = true
+    expect(true.xor(true)).to be_falsey
+  end
 
-    expect(p.xor(q)).to be_falsey
+  it 'false xor false should be false' do
+    expect(false.xor(false)).to be_falsey
+  end
+
+  it 'true then false should be false' do
+    expect(true.then(false)).to be_falsey
+  end
+
+  it 'true then true should be true' do
+    expect(true.then(true)).to be_truthy
+  end
+
+  it 'false then false should be true' do
+    expect(false.then(true)).to be_truthy
+  end
+
+  it 'not true should be false' do
+    expect(true.not).to be_falsey
+  end
+
+  it 'not false should be true' do
+    expect(false.not).to be_truthy
+  end
+
+  it 'true if and only if false should be false' do
+    expect(true.if_and_only_if(false)).to be_falsey
+  end
+
+  it 'true if and only if true should be true' do
+    expect(true.if_and_only_if(true)).to be_truthy
+  end
+
+  it 'false if and only if false should be true' do
+    expect(false.if_and_only_if(false)).to be_truthy
   end
 end
